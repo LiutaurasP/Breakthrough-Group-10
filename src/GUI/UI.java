8dc80051544f4153a254fafb9939ae5f7ad9fbdf
noid5454 <froyo5454@gmail.com>
2022-12-01 14:50:19 +0100

    Game frame changes

--- src/GUI/UI.java
+++ src/GUI/UI.java
@@ -2,14 +2,16 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 /**
  * A class that is the main frame of the UI. It contains board UI, menu (buttons,labels).
  */
-public class UI extends JFrame{
+public class UI extends SetUp{
+    JFrame frame;
     Object lock;
     BoardUI board;
-
+    JButton again;
     JLabel whoseTurn;
 
     /**
@@ -18,38 +20,62 @@ public class UI extends JFrame{
      */
     public UI(Object lock) {
         this.lock = lock;
+        frame = new JFrame();
         board = new BoardUI(lock);
-        setTitle("Menu.Breakthru");
-        setSize(770, 587);
-        getContentPane().setBackground(Color.BLACK);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setTitle("Play Breakthru");
+        frame.setSize(770, 587);
+        frame.setLocationRelativeTo(null);
+        frame.setBackground(Color.BLACK);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         //setResizable(false);
-        setVisible(true);
+        frame.setVisible(true);
 
-        getContentPane().add(board);
-        setBackground(Color.black);
+        frame.add(board);
+        frame.setBackground(Color.black);
         JPanel menu = new JPanel();
         whoseTurn = new JLabel("");
         whoseTurn.setFont(new Font("Monospaced",Font.BOLD,20));
         menu.add(whoseTurn);
         menu.setBorder(BorderFactory.createEmptyBorder(40,550,500,0));
+
+        //undo button
         JButton undo = new JButton("Undo Last");
         // TODO: UNDO LAST MOVE
         undo.addActionListener(e -> {
 
         });
         menu.add(undo);
+
+        //Quit button
         JButton quit = new JButton("Quit");
         quit.addActionListener(e -> {
             System.exit(0);
         });
         menu.add(quit);
+
+        //play again button
+        again = new JButton("Play Again?");
+        again.addActionListener(e -> {
+            frame.dispose();
+            choiceMenu = new GoldSilverChoice();
+        });
+        again.setVisible(false);
+        menu.add(again);
+
         menu.setBackground(Color.GRAY);
-        getContentPane().add(menu);
-
-
+        frame.add(menu);
+        frame.setIconImage(icon.getImage());
     }
+
+    /**
+     * Play again button getter.
+     * @return
+     */
+    public JButton getAgain() {
+        return again;
+    }
+
     /**
      * Turn label getter.
      * @return Returns turn label.
